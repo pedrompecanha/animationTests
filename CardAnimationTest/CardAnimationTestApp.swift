0753206556fad7269224aed3afe20b6db5f18796
Pedro <pedrompecanha27@gmail.com>
2023-03-03 20:06:58 -0300

    Initial Commit

---
+++ CardAnimationTest/CardAnimationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAnimationTestApp.swift
+//  CardAnimationTest
+//
+//  Created by Pedro Peçanha on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CardAnimationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
