24d166bd0dac9a255e8828f0e0d892f54383f96d
Pedro <pedrompecanha27@gmail.com>
2023-03-13 21:49:26 -0300

    Initial commit with the card animation.

--- CardAnimationTest/ContentView.swift
+++ CardAnimationTest/ContentView.swift
@@ -8,14 +8,111 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Namespace var namespace
+    @State var isFinalPosition = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            if !isFinalPosition {
+                ZStack{
+                    cardGrid
+                }
+            }
+            else {
+                VStack {
+                    cardLine
+                    Spacer()
+                }
+                .padding(.top, 60)
+            }
+            
+            VStack {
+                Spacer()
+                continueButton
+            }
         }
-        .padding()
+        .animation(.easeInOut, value: isFinalPosition)
+        
+    }
+    
+    var continueButton: some View {
+        Button(action: {
+            isFinalPosition.toggle()
+        }, label: {
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(.black)
+                    .frame(width: 200, height: 50)
+                Text("Continue")
+                    .foregroundColor(.white)
+            }
+            .padding(.bottom, 50)
+        })
+    }
+    
+    var cardGrid: some View {
+        VStack{
+            HStack{
+                cardTopLeft
+                cardTopRight
+            }
+            HStack{
+                cardBottomLeft
+                cardBottomRight
+                
+            }
+        }
+    }
+    
+    var cardLine: some View {
+        HStack(spacing: -30) {
+            cardTopLeft
+                .rotationEffect(Angle(degrees: -10))
+            cardTopRight
+                .rotationEffect(Angle(degrees: -5))
+                .offset(y: -20)
+            cardBottomLeft
+                .rotationEffect(Angle(degrees: 5))
+                .offset(y: -20)
+            cardBottomRight
+                .rotationEffect(Angle(degrees: 10))
+        }
+    }
+    
+    var cardTopLeft: some View {
+        Image("card")
+            .resizable()
+            .matchedGeometryEffect(id: "cardTopLeft", in: namespace)
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 80)
+            .ignoresSafeArea(.all, edges: .all)
+    }
+    
+    var cardBottomLeft: some View {
+        Image("card")
+            .resizable()
+            .matchedGeometryEffect(id: "cardBottomLeft", in: namespace)
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 80)
+            .ignoresSafeArea(.all, edges: .all)
+    }
+    
+    var cardTopRight: some View {
+        Image("card")
+            .resizable()
+            .matchedGeometryEffect(id: "cardTopRight", in: namespace)
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 80)
+            .ignoresSafeArea(.all, edges: .all)
+    }
+    
+    var cardBottomRight: some View {
+        Image("card")
+            .resizable()
+            .matchedGeometryEffect(id: "cardBottomRight", in: namespace)
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 80)
+            .ignoresSafeArea(.all, edges: .all)
     }
 }
 
