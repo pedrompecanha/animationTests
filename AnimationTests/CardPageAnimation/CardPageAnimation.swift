367efdd9ae568a998a655102fdc091de7de6da19
Pedro <pedrompecanha27@gmail.com>
2023-03-15 17:27:35 -0300

    Animation working with the 3d effect.

--- AnimationTests/CardPageAnimation/CardPageAnimation.swift
+++ AnimationTests/CardPageAnimation/CardPageAnimation.swift
@@ -14,12 +14,13 @@ struct CardPageAnimation: View {
     @Namespace var namespace
     @State var page: Int = 0
     @State var correctEdges = false
+    @State var pageController = 0
     var body: some View {
         ZStack {
             
 //            Color.green
             
-            if page == 0 {
+            if pageController == 0 {
                
                 ZStack {
                     cardStack
@@ -30,13 +31,13 @@ struct CardPageAnimation: View {
                 VStack {
                     RoundedRectangle(cornerRadius: 20)
                         .foregroundColor(.purple)
-                        .rotation3DEffect(correctEdges ? Angle(degrees: 50) : Angle(degrees: 50), axis: (x:10 ,y:0,z:0))
+                        .rotation3DEffect(correctEdges ? Angle(degrees: 0) : Angle(degrees: 50), axis: (x:10 ,y:0,z:0))
                         .matchedGeometryEffect(id: "card1", in: namespace)
                         .frame(width: 200, height: 50)
                         .padding(.top, 100)
                     Spacer()
                 }.onAppear{
-                    DispatchQueue.main.asyncAfter(deadline: .now() + animationDuration , execute: {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2 , execute: {
                         withAnimation {
                             correctEdges = true
                         }
@@ -66,13 +67,24 @@ struct CardPageAnimation: View {
 
 
             
-        }.animation(.easeInOut(duration: animationDuration), value: page)
+        }.animation(.easeInOut(duration: animationDuration), value: pageController)
             .onChange(of: page, perform: {_ in
-                if page == 0 {
-                    correctEdges = false
+                
+                
+                 if page == 0 {
+                     withAnimation {
+                         correctEdges = false
+                     }
+                 }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    pageController = page
                 }
+                
+                
+               
+                
             })
-        .background(.black)
+        .background(.primary)
            
     }
     
@@ -95,7 +107,7 @@ struct CardPageAnimation: View {
         
         var body: some View {
             RoundedRectangle(cornerRadius: 20)
-                .foregroundColor(.white)
+                .foregroundStyle(material)
                 .rotation3DEffect(rotationAngle, axis: (x:10 ,y:0,z:0))
                 .matchedGeometryEffect(id: geometryEffectID, in: namespace)
                 .frame(width: 150, height: 150, alignment: .center)
